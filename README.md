cf241495de8dcd38227dce63852228e6cbb839a4
albasoft-team <ibrahima-sory.diallo@albasoft.fr>
2020-09-10 14:19:39 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-xlsx-style-cakbms
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-xlsx-style-cakbms)
---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {ExcelService} from './excel.service';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular test';
+
+}
